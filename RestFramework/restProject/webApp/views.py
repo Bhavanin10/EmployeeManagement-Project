78ee0f909f8d86f749cefc93d2f56c2c200608e9
Bhavani N <bhavani.n@testyantra.in>
2020-03-04 13:08:33 +0530

    Rest Framework

---
+++ RestFramework/restProject/webApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from webApp.models import Student
+from .serializer import StudentSerializer
+
+# Create your views here.
+class StudentList(APIView):
+    def get(self,request):
+        s=Student.objects.all()
+        ser=StudentSerializer(s,many=True)
+        return Response(ser.data)
